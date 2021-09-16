68d9c7818f0b00b23e241ca757adb9290875fe2a
Andrew Erickson <aerickson@gmail.com>
2021-09-15 20:31:50 -0700

    initial commit from fork of octoprint-dht22

---
+++ README.md
@@ -0,0 +1,22 @@
+# OctoPrint-DHT22
+
+Reads DHT22 sensors and adds the data to OctoPrint's temperature data.
+
+## Setup
+
+Install libgpiod2.
+
+`sudo apt-get install libgpiod2`
+
+Install via the bundled [Plugin Manager](https://docs.octoprint.org/en/master/bundledplugins/pluginmanager.html)
+or manually using this URL:
+
+    https://github.com/aerickson/OctoPrint-DHT22/archive/master.zip
+
+## Graphing
+
+Install https://github.com/jneilliii/OctoPrint-PlotlyTempGraph to display the data.
+
+## Configuration
+
+**TODO:** Pin configuration is hard-coded currently.
---
+++ octoprint_DHT22/static/js/DHT22.js
@@ -0,0 +1,29 @@
+/*
+ * View model for OctoPrint-DHT22
+ *
+ * Author: Andrew J. Erickson
+ * License: AGPLv3
+ */
+$(function() {
+    function Dht22ViewModel(parameters) {
+        var self = this;
+
+        // assign the injected parameters, e.g.:
+        // self.loginStateViewModel = parameters[0];
+        // self.settingsViewModel = parameters[1];
+
+        // TODO: Implement your plugin's view model here.
+    }
+
+    /* view model class, parameters for constructor, container to bind to
+     * Please see http://docs.octoprint.org/en/master/plugins/viewmodels.html#registering-custom-viewmodels for more details
+     * and a full list of the available options.
+     */
+    OCTOPRINT_VIEWMODELS.push({
+        construct: Dht22ViewModel,
+        // ViewModels your plugin depends on, e.g. loginStateViewModel, settingsViewModel, ...
+        dependencies: [ /* "loginStateViewModel", "settingsViewModel" */ ],
+        // Elements to bind to, e.g. #settings_plugin_DHT22, #tab_plugin_DHT22, ...
+        elements: [ /* ... */ ]
+    });
+});
